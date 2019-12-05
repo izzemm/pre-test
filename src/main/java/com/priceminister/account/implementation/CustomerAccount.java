62779cb000b79286b6c27babc1c8ce0a5306a4bb
izzemm <58558449+izzemm@users.noreply.github.com>
2019-12-05 15:29:07 +0100

    Update CustomerAccount.java

--- src/main/java/com/priceminister/account/implementation/CustomerAccount.java
+++ src/main/java/com/priceminister/account/implementation/CustomerAccount.java
@@ -1,23 +1,38 @@
 package com.priceminister.account.implementation;
 
 import com.priceminister.account.*;
+import com.priceminister.account.IllegalBalanceException;
+import com.priceminister.account.IllegalDrawnAmountException;
+import com.priceminister.account.RuleNotInitializedException;
 
 
 public class CustomerAccount implements Account {
 
+    private Double balance = 0.0;
+
     public void add(Double addedAmount) {
-        // TODO Auto-generated method stub
+        balance = addedAmount;
     }
 
     public Double getBalance() {
-        // TODO Auto-generated method stub
-        return null;
+        return balance;
     }
 
-    public Double withdrawAndReportBalance(Double withdrawnAmount, AccountRule rule) 
-    		throws IllegalBalanceException {
-        // TODO Auto-generated method stub
-        return null;
+    public Double withdrawAndReportBalance(Double withdrawnAmount, AccountRule rule)
+            throws IllegalBalanceException, RuleNotInitializedException, IllegalDrawnAmountException {
+        if (withdrawnAmount <= 0) {
+            throw new IllegalDrawnAmountException(withdrawnAmount);
+        }
+        if (rule != null) {
+            Double resultingAccountBalance = balance - withdrawnAmount;
+            if (rule.withdrawPermitted(resultingAccountBalance)) {
+                balance = resultingAccountBalance;
+                return withdrawnAmount;
+            } else {
+                throw new IllegalBalanceException(withdrawnAmount);
+            }
+        } else {
+            throw new RuleNotInitializedException("Rule not initialized");
+        }
     }
-
 }
